82d88cce3615cf1d673cd85479b8d4b7b7c986e3
antriksh7 <126908787+antriksh7@users.noreply.github.com>
2024-06-08 13:14:48 -0400

    updated version

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,9 +1,3 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- * 
- */ 
 package ca.sheridancollege.project;
 
 /**
@@ -14,7 +8,7 @@ package ca.sheridancollege.project;
  */
 public abstract class Player {
 
-    private String name; //the unique name for this player
+    private String name; // the unique name for this player
 
     /**
      * A constructor that allows you to set the player's unique ID
--- src/ca/sheridancollege/project/WarPlayer.java
+++ src/ca/sheridancollege/project/WarPlayer.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ca.sheridancollege.project;
 
 /**
@@ -9,8 +5,22 @@ package ca.sheridancollege.project;
  * @author zhaob
  */
 public class WarPlayer extends Player {
-    
-     @Override
-    public void play(){};
-    
+
+    /**
+     * Constructor to create a WarPlayer with a unique name
+     *
+     * @param name the unique name to assign to this player.
+     */
+    public WarPlayer(String name) {
+        super(name); // Call the constructor of the superclass Player
+    }
+
+    /**
+     * Implement the abstract play method
+     */
+    @Override
+    public void play() {
+        // Implementation for the play method
+    }
+
 }
